733ca715e325b5fb350b4bd2372398c3b61bb33d
arista <arista@abramsons.info>
2025-01-29 22:39:37 +0000

    a bit more scaffolding

--- taterfarm-cli/src/ProjectGenerator.ts
+++ taterfarm-cli/src/ProjectGenerator.ts
@@ -4,5 +4,45 @@ export async function generateProject({
   projectDirectory: string
 }): Promise<{
 }> {
+  prepareDirectory({
+    projectDirectory
+  })
+  removeExtraneousFiles({
+    projectDirectory
+  })
+  addGitignore({
+    projectDirectory
+  })
+  return {}
+}
+
+
+async function prepareDirectory({
+  projectDirectory
+}: {
+  projectDirectory: string
+}): Promise<{
+}> {
+  // FIXME - implement this
+  return {}
+}
+
+async function removeExtraneousFiles({
+  projectDirectory
+}: {
+  projectDirectory: string
+}): Promise<{
+}> {
+  // FIXME - implement this
+  return {}
+}
+
+async function addGitignore({
+  projectDirectory
+}: {
+  projectDirectory: string
+}): Promise<{
+}> {
+  // FIXME - implement this
   return {}
 }
